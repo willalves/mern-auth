53f5792abfba53325701f00eded69026ecbeae35
William Silva <william@planner.net.br>
2024-03-25 19:33:39 +0100

    Add and connect to mongo database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(process.env.MONGO_PASS, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
 const app = express();
 
